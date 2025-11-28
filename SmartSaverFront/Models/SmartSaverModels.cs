8bea29cf92e3cc203d6af4ae0eb0182a79222671
Lorna Arwa <lornaarwa@gmail.com>
2025-11-28 23:52:46 +0300

    Add project files.

---
+++ SmartSaverFront/Models/SmartSaverModels.cs
@@ -0,0 +1,5 @@
+﻿namespace SmartSaverFront.Models
+{
+    public record Tx(DateTime Date, string Type, string Description, decimal Amount);
+    public record Goal(string Name, decimal Target, decimal Current);
+}
